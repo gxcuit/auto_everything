45ee90463476c2923fdb2ee946cd627a31bc3ab6
Gao Xiang <2018020488@qdu.edu.cn>
2020-01-03 13:14:53 +0800

    add error log

--- taobao_jfq.js
+++ taobao_jfq.js
@@ -2,25 +2,35 @@
  * @Author: gxcuit 
  * @Date: 2020-01-03 07:54:10 
  * @Last Modified by: gxcuit
- * @Last Modified time: 2020-01-03 10:27:36
+ * @Last Modified time: 2020-01-03 13:10:59
+ * @packagename: com.gxcuit.autotb.jfq
  */
 
+function toastError(error) {
+    console.error(error);
+    toast('error! '+error);
+}
+
+ 
 
 auto.waitFor();
+// console.setGlobalLogConfig({
+//     "file": "./auto_tb.txt"
+// });
 setScreenMetrics(device.width,device.height);
 
 if (!launch('com.taobao.taobao')) {
     toastLog('您安装淘宝了吗？');  
 }
 sleep(5000);
-console.log(currentPackage());
+//console.log(currentPackage());
 
-if (currentPackage()=='com.taobao.taobao') {
-    
-    
+if (currentPackage()=='com.taobao.taobao') {  
     click(800,1200);
     sleep(5000);
     click(970,1647);
+}else{
+    toastError('当前包名+'+currentPackage());
 }
 
 // 1. 先签到
@@ -33,14 +43,14 @@ if (qd) {
 
 //2. 浏览
 var go_view = textContains('去浏览').findOnce();
-console.log(go_view);
-
-// var close = className('android.widget.Button').clickable().find();
+if (!go_view) {
+    toastError("不存在‘去浏览’");
+}
 
 
 
 while (go_view) {
-    console.log(go_view);
+    console.log('当前浏览的是'+go_view);
     go_view.click(); 
     sleep(2000);
     scrollDown();
